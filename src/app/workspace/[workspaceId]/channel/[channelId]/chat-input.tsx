f0bd078dd18f8108e8b655bb1027551ecc950689
numdinkushi <numdinkushi@gmail.com>
2024-09-28 23:25:14 +0100

    wip: send message

--- src/app/workspace/[workspaceId]/channel/[channelId]/chat-input.tsx
+++ src/app/workspace/[workspaceId]/channel/[channelId]/chat-input.tsx
@@ -1,6 +1,10 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import dynamic from "next/dynamic";
 import Quill from 'quill';
+import { useCreateMessage } from '@/features/messages/api/use-create-message';
+import useWorkspaceId from '@/hooks/use-workspace-id';
+import useChannelId from '@/hooks/use-channel-id';
+import { toast } from 'sonner';
 
 interface ChatInputProps {
     placeholder: string;
@@ -9,14 +13,41 @@ interface ChatInputProps {
 const Editor = dynamic(() => import("@/components/editor"), { ssr: false });
 
 const ChatInput = ({ placeholder }: ChatInputProps) => {
+    const [editorKey, setEditorKey] = useState(0);
+    const [isPending, setIsPending] = useState(false);
     const editorRef = useRef<Quill | null>(null);
+    const workspaceId = useWorkspaceId();
+    const channelId = useChannelId();
+    const { mutate: createMessage } = useCreateMessage();
+
+    const handleSubmit = async ({
+        body,
+        image
+    }: { body: string, image: File | null; }) => {
+
+        try {
+            setIsPending(true);
+            await createMessage({
+                workspaceId,
+                channelId,
+                body,
+            }, { throwError: true });
+
+            setEditorKey((prevKey) => prevKey + 1);
+        } catch (error) {
+            toast.error('Failed to send message');
+        } finally {
+            setIsPending(false);
+        }
+    };
 
     return (
         <div className='px-5 w-full'>
             <Editor
+                key={editorKey}
                 placeholder={placeholder}
-                onSubmit={() => console.log('sfsf')}
-                disabled={false}
+                onSubmit={handleSubmit}
+                disabled={isPending}
                 innerRef={editorRef}
             />
         </div>
--- src/components/editor.tsx
+++ src/components/editor.tsx
@@ -28,7 +28,7 @@ interface EditorProps {
 
 const Editor = ({
     variant = 'create',
-    // onCancel,
+    onCancel,
     placeholder = 'Write something...',
     defaultValue = [],
     disabled = false,
@@ -75,8 +75,13 @@ const Editor = ({
                         enter: {
                             key: 'Enter',
                             handler: () => {
-                                // TODO: submit form
-                                return;
+                                const text = quill.getText();
+                                const addedImage = imageElementRef.current?.files?.[0] || null;
+                                const isEmpty = !addedImage && text.replace(/<(.|\n)*?>/g, "").trim().length === 0;
+
+                                if (isEmpty) return;
+                                const body = JSON.stringify(quill.getContents());
+                                submitRef.current?.({ body, image: addedImage });
                             }
                         },
                         shift_enter: {
@@ -120,7 +125,7 @@ const Editor = ({
         };
     }, [innerRef]);
 
-    const isEmpty = text.replace(/<(.|\n)*?>/g, "").trim().length === 0;
+    const isEmpty = !image && text.replace(/<(.|\n)*?>/g, "").trim().length === 0;
 
     const toggleToolbar = () => {
         setIsToolbarVisible((current) => !current);
@@ -147,7 +152,12 @@ const Editor = ({
                 ref={imageElementRef}
                 onChange={(event) => setImage(event.target.files![0])}
             />
-            <div className="flex flex-col border border-slate-200 rounded-md overflow-hidden focus-within:border-slate-300 focus-within:shadow-sm transition bg-white">
+            <div className={
+                cn(
+                    "flex flex-col border border-slate-200 rounded-md overflow-hidden focus-within:border-slate-300 focus-within:shadow-sm transition bg-white",
+                    disabled && 'opacity-50'
+                )}
+            >
                 <div className="h-full ql-custom" ref={containerRef} />
                 {
                     !!image && (
@@ -216,7 +226,7 @@ const Editor = ({
                                     disabled={disabled}
                                     size='sm'
                                     variant='outline'
-                                    onClick={() => { }}
+                                    onClick={onCancel}
                                 >
                                     Cancel
                                 </Button>
@@ -224,7 +234,12 @@ const Editor = ({
                                     disabled={disabled || isEmpty}
                                     size='sm'
                                     variant='outline'
-                                    onClick={() => { }}
+                                    onClick={() => {
+                                        onSubmit({
+                                            body: JSON.stringify(quillRef.current?.getContents()),
+                                            image,
+                                        });
+                                    }}
                                     className=' bg-[#007a5a] hover:bg-[#007a5a]/80 text-white'
                                 >
                                     Save
@@ -237,7 +252,12 @@ const Editor = ({
                         variant === 'create' && (
                             <Button
                                 disabled={disabled || isEmpty}
-                                onClick={() => { }}
+                                onClick={() => {
+                                    onSubmit({
+                                        body: JSON.stringify(quillRef.current?.getContents()),
+                                        image,
+                                    });
+                                }}
                                 className={cn('ml-auto', isEmpty
                                     ? 'bg-white hover:bg-white text-muted-foreground'
                                     : 'bg-[#007a5a] hover:bg-[#007a5a]/80 text-white',
